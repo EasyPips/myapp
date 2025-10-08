88b1aa8cf3ec5485becd6e87699b832b0ca93f0c
EasyPips <sundaybukunmi62@gmail.com>
2025-10-08 12:06:00 +0100

    comit

--- mybackend/api/views.py
+++ mybackend/api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import RegisterSerializer
+from .models import User
 
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class AdminOnlyView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        if request.user.role != 'admin':
+            return Response({'detail': 'Not authorized'}, status=403)
+        # Admin logic here
+        return Response({'message': 'Welcome, admin!'})
